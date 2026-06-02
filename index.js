8302f2bf67aa733c5739579aa05852d69d0ef479
Agung Ngurah <agungngurah017@gmail.com>
2026-06-03 02:14:52 +0700

    firtst

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const axios = require("axios");
+const token = process.env.DISCORD_TOKEN;
+const channelId = process.env.CHANNEL_ID;
+const { Client, GatewayIntentBits } = require('discord.js');
+const cron = require('node-cron');
 
-async function sendMessage() {
-    await axios.post(process.env.WEBHOOK_URL, {
-        content: "Black Market ges!"
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', async () => {
+    console.log(`Bot online sebagai ${client.user.tag}`);
+
+    const channel = await client.channels.fetch(channelId);
+
+    // Pesan saat bot online
+    channel.send('🤖 Bot sudah aktif.');
+
+    // Black Market: 01:00, 04:00, 07:00, 10:00, 13:00, 16:00, 19:00, 22:00
+    cron.schedule('0 1,4,7,10,13,16,19,22 * * *', async () => {
+        try {
+            await channel.send('@everyone 🚨 Black Market sudah refresh! Cek sekarang!');
+            console.log('Pesan Black Market terkirim');
+        } catch (err) {
+            console.error(err);
+        }
     });
 
-    console.log("Pesan terkirim");
-}
+    console.log('Jadwal Black Market aktif.');
+});
 
-sendMessage().catch(console.error);
+client.login(token);
